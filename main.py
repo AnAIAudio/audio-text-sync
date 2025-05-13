c56e8cceb92a7398c0dfa510d1ebe0b297cbf781
hanbyulkang <kanghanstar@gmail.com>
2025-05-13 15:13:59 +0900

    모듈 정리 및 불필요한 import 제거
    불필요한 import를 제거하여 코드를 간결화하고 가독성을 향상시켰습니다. 사용되지 않는 모듈과 함수들을 정리함으로써 유지보수성을 높였습니다.

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-from datetime import datetime
 import torch
-from sklearn.preprocessing import normalize
-import os
 from audio.cut_wave import (
     stt_using_whisper,
     whisper_text,
     whisper_srt,
-    cut_wav,
-    check_audio_sync,
 )
-from audio.wave_to_vector import run_wave2vec, audio_embed_list
-from compare import compare_dtw, seconds_to_srt_time, run_dtw
-from srt_utils.dtw_to_srt import run_dtw_to_srt
-from text.sentence_bert import run_sentence_bert, run_token_level_bert
 from text.text_util import create_text_line, split_sentences, SequentialPicker
-from text.word_bert import run_visualize, run_word_bert
 from utils.prepare import (
     prepare_directories,
     test_file_paths,
     read_text_files,
 )
-from utils.visualsize import visualize
 
 if __name__ == "__main__":
     torch.device("cuda" if torch.cuda.is_available() else "cpu")
