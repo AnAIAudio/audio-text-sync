ebec92a0ce86ca6c88f2c829dd2fa533faac3fed
HONGJAEHYEONG <secrett2633@kakao.com>
2025-05-21 05:24:19 +0000

    MFA 실행을 위해 audio, text, srt 을 저장하는 폴더 구조 변경
    audio, text, srt_utils 에 저장하던 구조에서 datasets 폴더안에 저장하도록 변경하였습니다.
    MFA 에서 학습하기 위해서 audio 와 text 은 하나의 폴더안에 같은 이름으로 저장해야 하기에 변경하였습니다.

--- utils/prepare.py
+++ utils/prepare.py
@@ -1,48 +1,40 @@
+import json
+
+
 def prepare_directories():
     import os
     from custom_path import MAIN_BASE_PATH
 
-    temp_directory_path = os.path.join(MAIN_BASE_PATH, "temp")
-    audio_directory_path = os.path.join(MAIN_BASE_PATH, "audio")
-    text_directory_path = os.path.join(MAIN_BASE_PATH, "text")
-    srt_directory_path = os.path.join(MAIN_BASE_PATH, "srt_utils")
+    dataset_directory_path = os.path.join(MAIN_BASE_PATH, "datasets", "S23")
 
     path_list = [
-        temp_directory_path,
-        audio_directory_path,
-        text_directory_path,
-        srt_directory_path,
+        dataset_directory_path,
     ]
 
     for path in path_list:
         if not os.path.exists(path):
             os.makedirs(path, exist_ok=True)
 
-    return (
-        temp_directory_path,
-        audio_directory_path,
-        text_directory_path,
-        srt_directory_path,
-    )
+    return dataset_directory_path
 
 
-def test_file_paths(
-    audio_directory_path: str,
-    text_directory_path: str,
-    srt_directory_path: str,
-):
+def test_file_paths(datasets_directory_path: str):
     import os
     from datetime import datetime
 
-    audio_file_path = os.path.join(audio_directory_path, "voix_result_mp3.mp3")
-    text_file_path = os.path.join(text_directory_path, "voix_result_txt.txt")
+    audio_file_path = os.path.join(datasets_directory_path, "S23.mp3")
+    json_file_path = os.path.join(datasets_directory_path, "S23.json")
+    text_file_path = os.path.join(datasets_directory_path, f"S23.txt")
     now = datetime.now()
     formatted = now.strftime("%Y%m%d%H%M%S")
-    srt_file_path = os.path.join(srt_directory_path, f"voix_result_srt_{formatted}.srt")
-    correct_srt_file_path = os.path.join(srt_directory_path, "correct_srt.srt")
+    srt_file_path = os.path.join(datasets_directory_path, f"S23_{formatted}.srt")
+    correct_srt_file_path = os.path.join(
+        datasets_directory_path, f"S23_{formatted}.srt"
+    )
 
     return (
         audio_file_path,
+        json_file_path,
         text_file_path,
         srt_file_path,
         correct_srt_file_path,
@@ -55,4 +47,10 @@ def read_text_files(text_file_path):
     with open(text_file_path, "r") as f:
         text = f.read()
 
+    return text
+
+
+def read_json_files(json_file_path):
+    with open(json_file_path, "r") as f:
+        text = json.load(f)
     return text
